76dcbf5c2d5a51215f0f398d5af5ceaac87bfbf7
daniloderiso <dderiso@gmail.com>
2020-02-10 14:43:13 +0100

    Add existing file

--- ecomphp.sql
+++ ecomphp.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.2
--- http://www.phpmyadmin.net
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Nov 08, 2017 at 06:41 PM
--- Server version: 10.1.16-MariaDB
--- PHP Version: 5.6.24
+-- Host: 127.0.0.1:3306
+-- Creato il: Feb 10, 2020 alle 13:37
+-- Versione del server: 8.0.18
+-- Versione PHP: 7.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -23,60 +25,67 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin`
+-- Struttura della tabella `admin`
 --
 
-CREATE TABLE `admin` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE IF NOT EXISTS `admin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `firstname` varchar(255) NOT NULL,
   `lastname` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `admin`
+-- Dump dei dati per la tabella `admin`
 --
 
 INSERT INTO `admin` (`id`, `firstname`, `lastname`, `email`, `password`) VALUES
-(1, 'Vivek', 'Vengala', 'vivek@codingcyber.com', '26e0eca228b42a520565415246513c0d');
+(1, 'Vivek', 'Vengala', 'vivek@codingcyber.com', '26e0eca228b42a520565415246513c0d'),
+(2, 'pippo', 'pluto', 'dderiso@gmail.com', 'dbfa726fb55fbe910834b1802785fa53');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `category`
+-- Struttura della tabella `category`
 --
 
-CREATE TABLE `category` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE IF NOT EXISTS `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `category`
+-- Dump dei dati per la tabella `category`
 --
 
 INSERT INTO `category` (`id`, `name`) VALUES
 (1, 'Mobiles'),
-(2, 'Cameras'),
-(3, 'Books');
+(4, 'Hardware'),
+(5, 'Software');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `orderitems`
+-- Struttura della tabella `orderitems`
 --
 
-CREATE TABLE `orderitems` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `orderitems`;
+CREATE TABLE IF NOT EXISTS `orderitems` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `pid` int(11) NOT NULL,
   `pquantity` varchar(255) NOT NULL,
   `orderid` int(11) NOT NULL,
-  `productprice` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `productprice` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `orderitems`
+-- Dump dei dati per la tabella `orderitems`
 --
 
 INSERT INTO `orderitems` (`id`, `pid`, `pquantity`, `orderid`, `productprice`) VALUES
@@ -93,20 +102,22 @@ INSERT INTO `orderitems` (`id`, `pid`, `pquantity`, `orderid`, `productprice`) V
 -- --------------------------------------------------------
 
 --
--- Table structure for table `orders`
+-- Struttura della tabella `orders`
 --
 
-CREATE TABLE `orders` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE IF NOT EXISTS `orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `uid` int(11) NOT NULL,
   `totalprice` varchar(255) NOT NULL,
   `orderstatus` varchar(255) NOT NULL,
   `paymentmode` varchar(255) NOT NULL,
-  `timestamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `timestamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `orders`
+-- Dump dei dati per la tabella `orders`
 --
 
 INSERT INTO `orders` (`id`, `uid`, `totalprice`, `orderstatus`, `paymentmode`, `timestamp`) VALUES
@@ -114,24 +125,27 @@ INSERT INTO `orders` (`id`, `uid`, `totalprice`, `orderstatus`, `paymentmode`, `
 (2, 2, '42060', 'Order Placed', 'cod', '2017-10-28 12:27:16'),
 (3, 6, '21980', 'Cancelled', 'cod', '2017-10-28 14:25:23'),
 (4, 6, '12965', 'In Progress', 'cod', '2017-10-28 14:28:29'),
-(5, 6, '7750', 'In Progress', 'cod', '2017-11-06 19:40:34');
+(5, 6, '7750', 'In Progress', 'cod', '2017-11-06 19:40:34'),
+(6, 7, '0', 'Order Placed', '', '2020-02-05 16:34:02');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `ordertracking`
+-- Struttura della tabella `ordertracking`
 --
 
-CREATE TABLE `ordertracking` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `ordertracking`;
+CREATE TABLE IF NOT EXISTS `ordertracking` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `orderid` int(11) NOT NULL,
   `status` varchar(255) NOT NULL,
   `message` text NOT NULL,
-  `timestamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `timestamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `ordertracking`
+-- Dump dei dati per la tabella `ordertracking`
 --
 
 INSERT INTO `ordertracking` (`id`, `orderid`, `status`, `message`, `timestamp`) VALUES
@@ -142,46 +156,50 @@ INSERT INTO `ordertracking` (`id`, `orderid`, `status`, `message`, `timestamp`) 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `products`
+-- Struttura della tabella `products`
 --
 
-CREATE TABLE `products` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE IF NOT EXISTS `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `catid` int(11) NOT NULL,
   `price` varchar(255) NOT NULL,
   `thumb` varchar(255) NOT NULL,
-  `description` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `description` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `products`
+-- Dump dei dati per la tabella `products`
 --
 
 INSERT INTO `products` (`id`, `name`, `catid`, `price`, `thumb`, `description`) VALUES
-(1, 'Canon EOS 1300D 18MP Digital SLR Camera (Black) with 18-55mm ISII Lens, 16GB Card and Carry Case', 2, '20990', 'uploads/Canon EOS 200D 24 2MP.jpg', 'The EOS 1300D packs in all the fun of photography, which is why we recommend it to users looking for their very first EOS DSLR camera. It uses an 18-megapixel APS-C size sensor and the DIGIC 4+ image processor'),
-(2, 'Sony DSC W830 Cyber-shot 20.1 MP Point and Shoot Camera (Black) with 8x Optical Zoom, Memory Card and Camera Case', 2, '7590', 'uploads/Sony Alpha a6000 Mirrorless Digital.jpg', 'The Sony DSC W830 Cyber-shot 20.1 MP Point and Shoot Camera (Black) with 8x Optical Zoom is a powerful camera full of features that put it at par with any professional DSLR. It is packed with a super HAD CCD sensor that comes with 20.1 effective megapixel'),
-(18, 'Sony Cyber-shot DSC-H300/BC E32 point & Shoot Digital camera ', 2, '12890', 'uploads/Sony Alpha ILCA-77M2Q 24 3MP.jpg', 'The High zoom camera Sony Cyber-shot H300, with a powerful 35x optical zoom, brings your subject to you for beautiful, precise pictures. A 20.1MP sensor, HD video and creative features, let you capture detailed images and movies with ease. A DSLR-style bo'),
-(19, 'General Knowledge 2018', 3, '16', 'uploads/General Knowledge 2018.jpg', 'An editorial team of highly skilled professionals at Arihant, works hand in glove to ensure that the students receive the best and accurate content through our books. From inception till the book comes out from print, the whole team comprising of authors,'),
-(20, 'The Power of your Subconscious Mind', 3, '99', 'uploads/The Power of your Subconscious Mind.jpg', 'It''s a very good n very useful book n it should be read by each n every one ...to knw the things that are not aware and know about the mind power .. Super duper book --ByAmazon Customeron 19 March 2017'),
-(21, 'Think and Grow Rich', 3, '75', 'uploads/Think and Grow Rich.jpg', 'An American journalist, lecturer and author, Napoleon Hill is one of the earliest producers of ''personal-success literature . As an author of self-help books, Hill has always abided by and promoted principle of intense and burning passion being the sole k');
+(22, 'MHIL - MICRO HARDWARE IN THE LOOP', 4, '1800', 'uploads/mhil-side.jpg', 'Micro Hardware in The Loop (MHIL) è un dispositivo elettromeccanico che permette di fare i test su unità di controllo elettronico generale. A seconda dell’obiettivo da raggiungere il sistema si collega ad una centralina: può essere collegato al controllo '),
+(23, 'M3', 4, '1950', 'uploads/m3-side.jpg', 'M3 è un prodotto concepito per testare sia applicazioni destinate a sistemi operativi specifici (Android – IOS – Windows Mobile) che applicazioni per web eseguite su Tablet o Smartphone.'),
+(24, 'EMS - Energy Monitoring System', 4, '4500', 'uploads/energy-monitoring-system-side.jpg', 'EMS è un sistema stand-alone pensato per il monitoraggio del consumo energetico rivolto sia ad aziende che privati. Tale dispositivo, attraverso un opportuno hardware pilotato da un microcontrollore, è in grado di rilevare i consumi di energia elettrica d'),
+(25, 'TMS - TEMPERATURE MONITORING SYSTEM', 4, '2580', 'uploads/temperature-monitoring-system-side.jpg', 'TMS è un sistema stand-alone pensato per il monitoraggio della temperatura indoor rivolto sia ad aziende che privati. Tale dispositivo, attraverso un opportuno hardware pilotato da un microcontrollore, è in grado di rilevare la temperatura del luogo in cu'),
+(26, 'C & P Utilities', 5, '1550', 'uploads/cep-utilities-side.jpg', 'C&P Utilities è un’applicazione per le aziende di gestione delle forniture acqua, elettricità e gas ai cittadini. Aziende caratterizzate da una struttura distribuita sul territorio che necessita di personale, in continuo movimento, che ne curi la manutenz'),
+(27, 'Portale Comunicazione e Collaborazione', 5, '580', 'uploads/portale-comunicazione-collaborazione-side.jpg', 'PCC (Portale Comunicazione e Collaborazione) è un’applicazione software di Content Management System (CMS), sviluppata con il prodotto SharePoint di Microsoft.\r\nLa sua funzione è soddisfare le esigenze di un’azienda di condividere informazioni e documenti');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `reviews`
+-- Struttura della tabella `reviews`
 --
 
-CREATE TABLE `reviews` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `reviews`;
+CREATE TABLE IF NOT EXISTS `reviews` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `pid` int(11) NOT NULL,
   `uid` int(11) NOT NULL,
   `review` text NOT NULL,
-  `timestamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `timestamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `reviews`
+-- Dump dei dati per la tabella `reviews`
 --
 
 INSERT INTO `reviews` (`id`, `pid`, `uid`, `review`, `timestamp`) VALUES
@@ -190,33 +208,36 @@ INSERT INTO `reviews` (`id`, `pid`, `uid`, `review`, `timestamp`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Struttura della tabella `users`
 --
 
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `timestamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `timestamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `users`
+-- Dump dei dati per la tabella `users`
 --
 
 INSERT INTO `users` (`id`, `email`, `password`, `timestamp`) VALUES
 (1, 'vivek@codingcyber.com', '26e0eca228b42a520565415246513c0d', '2017-10-27 12:05:10'),
-(2, 'vivek1@codingcyber.com', '$2y$10$cMzHNUFGKma96MywHmVMbekuJZb1tSNLsevHzLnSRbcRicQVhEC6a', '2017-10-27 12:24:25'),
-(6, 'vivek2@codingcyber.com', '$2y$10$apI7l.1wAS5pgbG4YfMrN.jNd5T3XmhecFuSV2M6UNdoUHImPXNxm', '2017-10-27 12:28:20');
+(2, 'dderiso@gmail.com', '$2y$10$KBMZMF9YdAXUWOC7Z5W6vu3Z0YoFN1vzaJrtjdvxZNHh.n4lKHAP6', '2020-02-05 16:33:09');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `usersmeta`
+-- Struttura della tabella `usersmeta`
 --
 
-CREATE TABLE `usersmeta` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `usersmeta`;
+CREATE TABLE IF NOT EXISTS `usersmeta` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `uid` int(11) NOT NULL,
   `firstname` varchar(255) NOT NULL,
   `lastname` varchar(255) NOT NULL,
@@ -227,158 +248,83 @@ CREATE TABLE `usersmeta` (
   `state` varchar(255) NOT NULL,
   `country` varchar(255) NOT NULL,
   `zip` varchar(255) NOT NULL,
-  `mobile` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `mobile` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `usersmeta`
+-- Dump dei dati per la tabella `usersmeta`
 --
 
 INSERT INTO `usersmeta` (`id`, `uid`, `firstname`, `lastname`, `company`, `address1`, `address2`, `city`, `state`, `country`, `zip`, `mobile`) VALUES
 (1, 2, 'Vivek', 'V', 'Coding Cyber', 'Hyd', 'Hyd', 'Hyderabad', 'Telangana', '', '500060', ''),
-(2, 6, 'Vivek', 'Vengala', 'Coding Cyber', '#201', 'DSNR', 'Hyderabad', 'Telangana', '', '500060', '9876543211');
+(2, 6, 'Vivek', 'Vengala', 'Coding Cyber', '#201', 'DSNR', 'Hyderabad', 'Telangana', '', '500060', '9876543211'),
+(3, 7, '', '', '', '', '', '', '', 'BD', '', '');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `wishlist`
+-- Struttura della tabella `wishlist`
 --
 
-CREATE TABLE `wishlist` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `wishlist`;
+CREATE TABLE IF NOT EXISTS `wishlist` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `pid` int(11) NOT NULL,
   `uid` int(11) NOT NULL,
-  `timestamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `timestamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `wishlist`
+-- Dump dei dati per la tabella `wishlist`
 --
 
 INSERT INTO `wishlist` (`id`, `pid`, `uid`, `timestamp`) VALUES
 (1, 1, 6, '2017-10-30 16:36:45'),
 (2, 2, 6, '2017-10-30 16:38:07'),
-(3, 21, 6, '2017-11-06 19:42:35');
+(3, 21, 6, '2017-11-06 19:42:35'),
+(4, 22, 2, '2020-02-05 19:24:31'),
+(5, 24, 2, '2020-02-05 19:24:43'),
+(6, 24, 2, '2020-02-05 20:03:14');
+
+-- --------------------------------------------------------
 
 --
--- Indexes for dumped tables
+-- Struttura della tabella `wishlistname`
 --
 
---
--- Indexes for table `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id`);
+DROP TABLE IF EXISTS `wishlistname`;
+CREATE TABLE IF NOT EXISTS `wishlistname` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pid` int(11) NOT NULL,
+  `uid` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `timestamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
 
 --
--- Indexes for table `category`
---
-ALTER TABLE `category`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `orderitems`
---
-ALTER TABLE `orderitems`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `orders`
---
-ALTER TABLE `orders`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `ordertracking`
---
-ALTER TABLE `ordertracking`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `products`
---
-ALTER TABLE `products`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `reviews`
---
-ALTER TABLE `reviews`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
--- Indexes for table `usersmeta`
---
-ALTER TABLE `usersmeta`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `wishlist`
---
-ALTER TABLE `wishlist`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
+-- Dump dei dati per la tabella `wishlistname`
 --
 
---
--- AUTO_INCREMENT for table `admin`
---
-ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `category`
---
-ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT for table `orderitems`
---
-ALTER TABLE `orderitems`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
---
--- AUTO_INCREMENT for table `orders`
---
-ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
---
--- AUTO_INCREMENT for table `ordertracking`
---
-ALTER TABLE `ordertracking`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
---
--- AUTO_INCREMENT for table `products`
---
-ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
---
--- AUTO_INCREMENT for table `reviews`
---
-ALTER TABLE `reviews`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
---
--- AUTO_INCREMENT for table `usersmeta`
---
-ALTER TABLE `usersmeta`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `wishlist`
---
-ALTER TABLE `wishlist`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+INSERT INTO `wishlistname` (`id`, `pid`, `uid`, `nome`, `timestamp`) VALUES
+(1, 1, 1, 'alfa', '2017-10-30 16:36:45'),
+(2, 2, 1, 'alfa', '2017-10-30 16:38:07'),
+(3, 21, 1, 'beta', '2017-11-06 19:42:35'),
+(4, 22, 2, 'alfa', '2020-02-05 19:24:31'),
+(5, 24, 2, 'alfa', '2020-02-05 19:24:43'),
+(6, 24, 2, 'alfa', '2020-02-05 20:03:14'),
+(9, 22, 2, 'beta', '2020-02-06 19:58:27'),
+(10, 22, 2, 'gamma', '2020-02-06 20:04:13'),
+(23, 24, 2, 'prova1', '2020-02-06 21:07:57'),
+(24, 24, 2, 'gamma', '2020-02-06 21:10:21'),
+(25, 23, 2, 'gamma', '2020-02-06 21:12:13'),
+(26, 23, 2, 'beta', '2020-02-06 21:17:15'),
+(27, 23, 2, 'prova1', '2020-02-07 10:10:44'),
+(28, 23, 2, 'beta', '2020-02-07 10:12:02');
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
